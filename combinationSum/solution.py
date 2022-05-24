2143b2cd073a93ac16c8913d5ef37c2633c1adb6
fabio <fabiocosta07@gmail.com>
2022-05-24 13:00:25 -0300

    combination sum add test verification

--- combinationSum/solution.py
+++ combinationSum/solution.py
@@ -3,3 +3,4 @@ from typing import List
 # https://www.youtube.com/watch?v=GBKI9VSKdGg
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+      return target
--- combinationSum/test_solution.py
+++ combinationSum/test_solution.py
@@ -1,3 +1,4 @@
+from typing import List
 import unittest
 
 from solution import Solution
@@ -7,8 +8,19 @@ class TestSol(unittest.TestCase):
 
     def test_sol1(self):
         sol = Solution()
-        res = sol.combinationSum([2,3,6,7], 7)
-        self.assertEqual(res, 1, "Should be ")
+        res = sol.combinationSum([2,3,6,7], 7)        
+        self.assertTrue(isEqual(res, [[2,2,3],[7]]), "Should be [[2,2,3],[7]]")
+
+def isEqual(l1: List[List[int]], l2: List[List[int]]) -> bool:
+    for c1 in l1:
+        if not hasCombinationSum(c1, l2):
+            return False
+    return True
+def hasCombinationSum(c1: List[int], l: List[List[int]]) -> bool:
+    for c in l:
+        if sum(c) == sum(c1):
+            return True
+    return False
 
 if __name__ == '__main__':
     unittest.main()
